8be1b771ffa78f406140f5dce2e70004711d03f2
ateBitDev <raul.santiago.aguirre@gmail.com>
2019-02-05 23:24:03 -0800

    Update files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/index.css';
+import config from './config.js';
+import axios from 'axios';
+import {
+  Route,
+  Link,
+  NavLink,
+  BrowserRouter
+} from 'react-router-dom'
+import NotFound from './Components/NotFound'
+
+
+//Importing Components
+import Header from './Components/Header';
 
 class App extends Component {
+
+constructor()
+{
+  super()
+
+  this.state = {
+    pics:[],
+    query: "Cats"
+  }
+}
+
+componentDidMount()
+{
+  this.performSearch(this.state.query);
+}
+
+handleTag = e => {
+  e.preventDefault();
+  this.performSearch(e.target.textContent);
+}
+
+
+
+performSearch = (query) =>
+{
+  axios.get(` https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0e643e2fbfda782d581f49f99861bb54&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+  .then(response => {
+      this.setState({
+          pics: response.data.photos.photo
+        })
+        console.log(this.state.pics);
+        this.setState({query : query});
+    })
+    .catch(error => 
+      {
+        console.log('Error fetching and parsing data', error);
+      });
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <BrowserRouter>
+      <div className="container">
+        <Header tag={this.handleTag} pics={this.state.pics} query={this.state.query} search={this.performSearch} />
+        <Route component={ NotFound }/>
       </div>
+    </BrowserRouter>
     );
   }
 }
